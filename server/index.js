2c6903390998909cba7b18129d3385dd7ae9cf82
Jai-0828 <mailsforjaii@gmail.com>
2026-09-15 12:28:15 +0530

    fix: correct import path case for connectdb.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDb from './config/connectDb.js';
+import connectDb from './config/connectdb.js';
 dotenv.config()
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
