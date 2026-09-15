84e7e9846d3119b7c65594a356d81304ece9d17b
Jai Prakash Yadav <jaiy082528@gmail.com>
2026-09-15 13:01:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interviewer-client-9fj8.onrender.com",
     credentials: true
 }))
 
