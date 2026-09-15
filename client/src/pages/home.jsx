863f9d2e7e0fca4c4d8e43a51c738c2cacf27591
Jai Prakash Yadav <jaiy082528@gmail.com>
2026-09-15 12:50:25 +0530

    Fix import case for Navbar component

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 import { useSelector } from 'react-redux'
 import { motion } from 'motion/react';
 import {
